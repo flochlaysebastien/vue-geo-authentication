ab15481a0340bd266706fb37af0a39463eef5178
Flochlay <sebastien.flochlay@gmail.com>
2020-04-10 02:07:37 +0200

    feat(home): add a navigation bar component

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="" class="brand-logo left">GeoAuth</a>
+        <ul class="right">
+            <li><a href=""></a>Signup</li>
+            <li><a href=""></a>Login</li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+
+</style>

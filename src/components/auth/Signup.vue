b74408ad59298d7d1bfdfc73b7d026cd622cf4dc
Flochlay <sebastien.flochlay@gmail.com>
2020-04-12 01:01:51 +0200

    feat(auth): create a signup view

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>Signup view</div>
+  <div class="signup container">
+    <form class="card-panel">
+      <h2 class="center deep-purple-text">Signup</h2>
+      <div class="field">
+        <label for="email">Email</label>
+        <input id="email" type="email" />
+      </div>
+      <div class="field">
+        <label for="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <div class="field">
+        <label for="name">Alias</label>
+        <input id="name" type="text" />
+      </div>
+      <div class="field center">
+        <button class="btn deep-purple">Signup</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -12,4 +31,14 @@ export default {
 </script>
 
 <style>
+.signup{
+  max-width: 400px;
+  margin-top: 60px;
+}
+.signup h2{
+  font-size: 2.4em;
+}
+.signup .field{
+  margin-bottom: 16px;
+}
 </style>
